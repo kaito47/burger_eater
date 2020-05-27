66e483b1c76606086324f0369c0b8be74d816061
Kait OShaughnessy <oshaughnessykm@gmail.com>
2020-05-27 15:43:18 -0400

    added routes to controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,7 +4,45 @@ const router = express.Router();
 const burger_model = require('../models/burger.js');
 
 //Sets up Routes
+router.get('/', function (req, res) {
+    burger.all(function (data) {
+        let hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    })
+});
+
+router.post('/', function (req, res) {
+    burger.create([
+        req.body.burger_name
+    ], function (result) {
+        // Send back the ID of the new burger
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put('/', function (req, res) {
+    // QUESTION: because this has the condition as the id should I remove the id in the orm method?
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 
 // Exports router
-module.exports = router; 
+module.exports = router;
+
+// QUESTION: DID NOT SEE THE URL CHANGE IN THE DEMO - CAN THEY ALL BE AT THE ROUTE???
