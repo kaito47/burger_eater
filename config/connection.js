a648bf84cc2c0c3d126450bcb6053b2002b843a6
Kait OShaughnessy <oshaughnessykm@gmail.com>
2020-05-27 22:50:27 -0400

    updated connection file to enable mysql connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,20 @@
 // Sets up MySQL connection
 const mysql = require("mysql");
+let connection;
 // Sets up secure password
 require("dotenv").config();
 
-const connection = mysql.createConnection({
-    host: "s3lkt7lynu0uthj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "h4yhcel14myjligw",
-    password: "e49ezlv6meuqfiz3",
-    database: "a5hhg5rjtebx660u"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.DB_PASSWORD,
+        database: "burgers_db"
+    });
+};
 // Makes connection to MySQL
 connection.connect(function (err) {
     if (err) {
