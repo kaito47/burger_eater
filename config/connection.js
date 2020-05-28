ff7fbcf7b9899fbb7ff221e39470550449c130e3
Kait OShaughnessy <oshaughnessykm@gmail.com>
2020-05-27 23:52:35 -0400

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    const connection = mysql.createConnection({
+    connection = mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root",
